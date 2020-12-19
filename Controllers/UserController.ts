0b3f487d57d11255c79b95f20f4b383a4990669b
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-12-19 20:58:37 +0330

    crate register user controller => UserController

--- Controllers/UserController.ts
+++ Controllers/UserController.ts
@@ -1,12 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 import User from './../Models/UserModel';
+import bcrypt from 'bcryptjs';
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { username, password } = await req.body;
-		const newUser = await new User({
+
+		//validator
+
+		const schema = Joi.object({
+			username: Joi.string().min(6).max(30).required(),
+			password: Joi.string().min(6).max(30).required(),
+		});
+
+		const result = await schema.validate({
 			username,
 			password,
 		});
+
+		if (result.error) {
+			res.status(400).json({
+				message: result.error.details[0].message,
+			});
+		}
+
+		//hash password
+
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hashSync(password, salt);
+
+		const newUser = await new User({
+			username,
+			password: hashPassword,
+		});
+
 		newUser.save();
 		res.status(200).json(newUser);
 
@@ -16,4 +44,11 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 	}
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+	} catch (err) {
+		console.log('ERORR : ', err);
+	}
+};
+
+export { createUser, loginUser };
--- Routes/UserRoute.ts
+++ Routes/UserRoute.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { createUser } from '../Controllers/UserController';
+import { createUser, loginUser } from '../Controllers/UserController';
 const userRouter: Router = Router();
 
 userRouter.post('/register', createUser);
+userRouter.post('/login', createUser);
 
 export default userRouter;
